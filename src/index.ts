eb17b02706d4054cba536c1c5a5a33e290b6cbbf
root <root@juraj-Latitude-5310>
2022-08-21 18:09:33 +0100

    Get data visible

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { Flat, FlatDb } from './types';
 import pg from 'pg';
+import { renderPage } from './pageTemplate.js';
 const { Client } = pg;
 
 // psql -h localhost -p 5432 -U postgres
 
 const PAGE_SIZE = 20;
+const N_APARTMENTS = 500;
+const N_PAGES = N_APARTMENTS / PAGE_SIZE;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 const PORT = 3000;
@@ -22,18 +26,21 @@ const client = new Client({
 });
 
 app.get('/:page?', async (req, res) => {
-  let pageOffset = req.params.page ? (Number(req.params.page) - 1) * PAGE_SIZE : 0;
+  const page: number = req.params.page ? Number(req.params.page) : 1;
+  let pageOffset = (page - 1) * PAGE_SIZE;
   const response = await client.query(`SELECT * from flats LIMIT ${PAGE_SIZE} OFFSET ${pageOffset};`);
   const flats = response.rows as FlatDb[];
 
-  res.send({
-    flats: flats.map((flat) => ({
-      id: flat.id,
-      name: flat.name,
-      price: flat.price,
-      location: flat.location,
-    })),
-  });
+  // res.send({
+  //   flats: flats.map((flat) => ({
+  //     id: flat.id,
+  //     name: flat.name,
+  //     price: flat.price,
+  //     location: flat.location,
+  //   })),
+  // });
+
+  res.send(renderPage(flats, page));
 });
 
 app.post('/flat', async (req, res) => {
---
+++ src/pageTemplate.ts
@@ -0,0 +1,44 @@
+import { FlatDb } from './types';
+
+const N_PAGES = 25;
+
+const paging = (page: number) => {
+  return `
+    ${page - 1 > 0 ? `<a href="/${page - 1}">Previous page</a>` : ''}
+    ${page + 1 <= N_PAGES ? `<a href="/${page + 1}">Next page</a>` : ''}
+  `;
+};
+
+export const renderPage = (flats: FlatDb[], page: number): string => {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+        <title>Flats for sale</title>
+    </head>
+    <body>
+        <h1>Flats for sale</h1>
+        <p>Test</p>
+
+        ${paging(page)}
+        <div class="container">
+          ${flats
+            .map((flat) => {
+              return `
+                  <div class="flat">
+                      <span class="name">${flat.name}</span>
+                      <span class="price">${flat.price}</span>
+                      <p class="location">${flat.location}</p>
+                  </div>
+              `;
+            })
+            .join('')}
+        </div>
+        ${paging(page)}
+    </body>
+    </html>
+  `;
+};
