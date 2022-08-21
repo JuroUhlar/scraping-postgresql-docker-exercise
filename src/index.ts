463aef5697c76c41b36e71b856814f95732e6428
root <root@juraj-Latitude-5310>
2022-08-21 14:48:31 +0100

    Don't track DB content in git, instead seed it on app startup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { Flat } from "./types";
-import pg from "pg";
+import { seedDatabase } from './seedDatabase.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Flat } from './types';
+import pg from 'pg';
 const { Client } = pg;
 
 // psql -h localhost -p 5432 -U postgres
@@ -13,40 +14,42 @@ app.use(bodyParser.json());
 const PORT = 3000;
 
 const client = new Client({
-  password: "postgres",
-  user: "postgres",
-  host: "postgres",
+  password: 'postgres',
+  user: 'postgres',
+  host: 'postgres',
 });
 
-app.get("/", async (req, res) => {
-  const users = await client.query("SELECT * from flats;");
+app.get('/', async (req, res) => {
+  const users = await client.query('SELECT * from flats;');
 
-  console.log("Get");
+  console.log('Get');
 
   res.send({
     flats: users.rows,
   });
 });
 
-app.post("/flat", async (req, res) => {
+app.post('/flat', async (req, res) => {
   const { name, location, price, imgUrls }: Flat = req.body;
   // `INSERT INTO flats (name, price, location, images) VALUES('Dom', '3 000 000 Kč', 'Praha', '["imageUrl.test.com"]');`;
   await client.query(
-    "INSERT INTO flats (name, price, location, images) VALUES ($1, $2, $3, $4)",
+    'INSERT INTO flats (name, price, location, images) VALUES ($1, $2, $3, $4)',
     [name, price, location, imgUrls.toString()],
     (error) => {
       if (error) {
-        res.status(400).send("Malformed request");
+        res.status(400).send('Malformed request');
         throw error;
       }
-      res.status(201).send("Flat added");
-    }
+      res.status(201).send('Flat added');
+    },
   );
 });
 
 (async () => {
   await client.connect();
+  await seedDatabase(client);
+
   app.listen(PORT, () => {
-    console.log("Started at http://localhost:%d", PORT);
+    console.log('Started at http://localhost:%d', PORT);
   });
 })();
---
+++ src/seedDatabase.ts
@@ -0,0 +1,31 @@
+import pg from 'pg';
+import fs from 'fs';
+import { Flat } from './types';
+
+export const seedDatabase = async (client: pg.Client) => {
+  // Create table if necessary
+  const result = await client.query(
+    `CREATE TABLE IF NOT EXISTS "flats" ("id" SERIAL, "name" VARCHAR(300) NOT NULL, "price" VARCHAR(300) NOT NULL, "location" VARCHAR(300) NOT NULL, "images" VARCHAR(2000) NOT NULL, PRIMARY KEY ("id"));`,
+  );
+  if (result) {
+    console.log('Table created or already exists.');
+  }
+
+  const flats = await client.query('SELECT * from flats;');
+  if (flats.rows.length === 0) {
+    let flatData = JSON.parse(fs.readFileSync('data/flats.json', { encoding: 'utf8' })) as Flat[];
+
+    flatData.forEach(async ({ name, location, price, imgUrls }, index) => {
+      await client.query(
+        'INSERT INTO flats (name, price, location, images) VALUES ($1, $2, $3, $4)',
+        [name, location, price, JSON.stringify(imgUrls)],
+        (error) => {
+          if (error) {
+            console.log(error);
+          }
+          console.log(`Flat n. ${index + 1} added to database`);
+        },
+      );
+    });
+  }
+};
